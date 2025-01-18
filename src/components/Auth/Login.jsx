46e4ff5d0cd460d39bd5c608ef7cd4b09f5cc1f2
Harsh Agarwal <Realharshagarwal@gmail.com>
2025-01-18 23:49:57 +0530

    Setup Basic Structure

---
+++ src/components/Auth/Login.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Login = () => {
+  return (
+    <div className='flex item-center justify-center'>
+      <div className='border-2 border-red-600'>
+        <form>
+          <input type="email" placeholder='Email' />
+          <input type="password" placeholder="Password" />
+        </form>sl;
+      </div>
+      
+    </div>
+  );
+}
+
+export default Login;
