8d9f012ea2b04bee79dd569a24a4374399dc4680
Harsh Agarwal <Realharshagarwal@gmail.com>
2025-01-21 18:33:54 +0530

    Made somr modifications

--- src/components/Subcomponents/TaskListNumbers.jsx
+++ src/components/Subcomponents/TaskListNumbers.jsx
@@ -23,14 +23,7 @@ const TaskListNumbers = () => {
             <h2 className='text-2xl font-semibold'>0</h2>
             <h3 className='text-xl font-medium'>New Task</h3>
         </div>
-        <div className='w-[45%] py-6 px-9 bg-green-400 rounded-xl'>
-            <h2 className='text-2xl font-semibold'>0</h2>
-            <h3 className='text-xl font-medium'>New Task</h3>
-        </div>
-        <div className='w-[45%] py-6 px-9 bg-yellow-400 rounded-xl'>
-            <h2 className='text-2xl font-semibold'>0</h2>
-            <h3 className='text-xl font-medium'>New Task</h3>
-        </div>
+       
     </div>
   );
 }
